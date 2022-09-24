c4bb4f3e98b2c0c19a74926fd3958003aaaf99b5
Abhijeet98s <abhi.21smiley00@gmail.com>
2022-09-24 15:47:04 +0530

    changes in js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 let ques = document.getElementById("question")
 let options = document.querySelectorAll(".options")
-let option1 = document.getElementById("opt1")
+let optionContainer = document.querySelectorAll(".option")
 let inputs = document.querySelectorAll(".input")
 let confirmBtn = document.getElementById("confirm")
 let form = document.getElementById("form")
@@ -8,40 +8,99 @@ let form = document.getElementById("form")
 // console.log(inputs);
 // console.log(options[0].textContent)
 
+let initialValue = 0;
+let data1;
+let answer;
+
 fetch("https://opentdb.com/api.php?amount=10&type=multiple")
     .then(res => res.json())
     .then(data => {
-        // console.log(data)
-
-        // getting questions from api
-        ques.innerText = data.results[0].question
-        let allOpt = [];
-
-        // putting all options in new array
-        allOpt.push(data.results[0].correct_answer)
-        allOpt.push(data.results[0].incorrect_answers)
-        // console.log(allOpt.flat());
-
-        // flatting the above array
-        let finalAllOpt = allOpt.flat();
-
-        //setting the textcontent for options
-        for (let i = 0; i < options.length; i++) {
-            options[i].textContent = finalAllOpt[i]
-        }
-
-        for (let i = 0; i < inputs.length; i++) {
-            inputs[i].setAttribute("value", `${finalAllOpt[i]}`);
-            console.log(inputs[i].value)
-        }
-        // console.log(inputs[0]);
-
-        // submitting the answers
-        confirmBtn.addEventListener("click", (event) => {
-            event.preventDefault();
-            const answer = document.querySelector('input[name="fav_language"]:checked').value;
-            console.log(answer)
-        })
+        data1 = data
+        render(data1)
     })
 
 
+function render(data1) {
+    // console.log(data)
+
+    // getting questions from api
+    ques.innerText = data1.results[initialValue].question
+    let allOpt = [];
+
+    // putting all options in new array
+    allOpt.push(data1.results[initialValue].correct_answer)
+    allOpt.push(data1.results[initialValue].incorrect_answers)
+
+    // console.log(allOpt);
+
+    // flatting the above array
+    let finalAllOpt = allOpt.flat();
+
+    // console.log(finalAllOpt)
+    // console.log(data.results[initialValue].correct_answer)
+
+
+    optionContainer[0].style.backgroundColor = "#2d035e"
+
+    // console.log(data.results[initialValue].correct_answer);
+
+    //setting the textcontent for options
+    for (let i = 0; i < options.length; i++) {
+        options[i].textContent = finalAllOpt[i]
+    }
+
+
+    // setting value attributes for the input element
+    for (let i = 0; i < inputs.length; i++) {
+        inputs[i].setAttribute("value", `${finalAllOpt[i]}`);
+        // console.log(inputs[i].value)
+    }
+    // console.log(inputs[0]);
+  
+
+    // submitting the answers
+    confirmBtn.addEventListener("click", (event) => {
+        event.preventDefault();
+        answer = document.querySelector('input[name="fav_language"]:checked').value;
+        console.log(answer == finalAllOpt[0])
+
+        // allOpt = [];
+        // finalAllOpt = [];
+        // initialValue++;
+
+        if (answer == finalAllOpt[0]) {
+            console.log("correct answer")
+            setTimeout(() => {
+                initialValue++;
+                render(data1);
+            }, 1000);
+        }
+        else {
+            console.log("incorrect answer");
+        }
+
+        // render(data1);
+        // checking the answer
+        //     if (answer == finalAllOpt[0]) {
+        //         // console.log("correct answer");
+        //         optionContainer[0].style.backgroundColor = "green"
+
+        //         setTimeout(() => {
+        //             initialValue++;
+        //             // optionContainer[initialValue].style.backgroundColor = ""
+        //             allOpt = [];
+        //             finalAllOpt = [];                              
+        //             render(data)
+
+        //         }, 1000);
+
+        //     }
+
+        //     else {
+        //         alert("Incorrect Answer")
+        //         // console.log("wrong answer");
+        //         optionContainer[0].style.backgroundColor = "green"
+        //     }
+
+    })
+}
